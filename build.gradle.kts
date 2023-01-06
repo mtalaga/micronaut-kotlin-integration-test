77fa960b03d23a3d73d220eee50fbcb7467cbabb
Michal Talaga <6644307+mtalaga@users.noreply.github.com>
2023-01-06 17:08:11 +0100

    Bumped versions: Kotlin - 1.8.0 Micronaut - 3.8.0 Gradle - 7.6 JDK - 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("org.jetbrains.kotlin.kapt") version "1.6.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.kapt") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.micronaut.application") version "3.4.1"
+    id("io.micronaut.application") version "3.6.7"
 }
 
 version = "0.1"
-group = "example"
+group = "pl.mt"
 
-val kotlinVersion = project.properties.get("kotlinVersion")
 repositories {
     mavenCentral()
 }
+
+val kotlinVersion = project.properties["kotlinVersion"]
 
 dependencies {
     kapt("io.micronaut:micronaut-http-validation")
@@ -24,8 +25,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     runtimeOnly("ch.qos.logback:logback-classic")
     implementation("io.micronaut.mongodb:micronaut-mongo-sync")
-    annotationProcessor("io.micronaut.serde:micronaut-serde-processor:1.3.2")
-    implementation("io.micronaut.serde:micronaut-serde-jackson:1.3.2")
+    annotationProcessor("io.micronaut.serde:micronaut-serde-processor:1.5.0")
+    implementation("io.micronaut.serde:micronaut-serde-jackson:1.5.0")
 
     annotationProcessor("io.micronaut.data:micronaut-data-document-processor")
 
@@ -36,18 +37,18 @@ application {
     mainClass.set("example.ApplicationKt")
 }
 java {
-    sourceCompatibility = JavaVersion.toVersion("11")
+    sourceCompatibility = JavaVersion.toVersion("19")
 }
 
 tasks {
     compileKotlin {
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "19"
         }
     }
     compileTestKotlin {
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "19"
         }
     }
 }
